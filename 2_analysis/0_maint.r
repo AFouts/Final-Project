2d4070be7457b1df13ad016994a9ee67e0584dd6
AFouts <adrien_foutelet@brown.edu>
2024-04-16 13:36:22 -0400

    Started to retructure the code as expected

---
+++ 2_analysis/0_maint.r
@@ -0,0 +1,15 @@
+# Restore renv environment (should happen automatically)
+renv::restore()
+
+## Data Cleaning
+source("2_analysis/1_clean-data.r")
+
+## Analysis
+source("2_analysis/2_run-regressions.r")
+
+## Figures & Tables
+source("2_analysis/3_tables.r")
+source("2_analysis/4_figures.r")
+
+## Run Tests
+testthat::test_dir("2_analysis/0_tests")
